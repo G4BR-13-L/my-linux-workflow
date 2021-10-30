ce841b921894470a4db80b01264aaa45653fc55e
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-10-30 12:10:24 -0300

    Update script

--- gearingUp.sh
+++ gearingUp.sh
@@ -29,19 +29,19 @@ sudo rm /var/lib/dpkg/lock-frontend;
 sudo rm /var/cache/apt/archives/lock;
 
 ## Atualizando o repositório ##
-sudo apt update -y
+sudo apt update -y;
 
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_VSCODE"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_MICROSOFT_TEAMS"     -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"     -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VSCODE"              -P "$DIRETORIO_DOWNLOADS";
+wget -c "$URL_MICROSOFT_TEAMS"     -P "$DIRETORIO_DOWNLOADS";
+wget -c "$URL_GOOGLE_CHROME"     -P "$DIRETORIO_DOWNLOADS";
 
 
 
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb;
 
 
 # Instalar programas no apt
@@ -54,11 +54,12 @@ for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
 done
 
 ## Instalando pacotes Flatpak ##
-flatpak install flathub com.spotify.Client
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub org.telegram.desktop
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.jgraph.drawio.desktop
+flatpak install flathub com.spotify.Client;
+flatpak install flathub com.bitwarden.desktop;
+flatpak install flathub org.telegram.desktop;
+flatpak install flathub com.discordapp.Discord;
+flatpak install flathub com.jgraph.drawio.desktop;
+flatpak install flathub com.bitwarden.desktop;
 
 ## Desenvolvimento
 ### NODEJS
@@ -88,8 +89,9 @@ sudo apt install openjdk-default;
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
-sudo apt update && sudo apt dist-upgrade -y
-flatpak update
-sudo apt autoclean
-sudo apt autoremove -y
+sudo apt update && sudo apt dist-upgrade -y;
+sudo pkcon update;
+flatpak update;
+sudo apt autoclean;
+sudo apt autoremove -y;
 # ---------------------------------------------------------------------- #
