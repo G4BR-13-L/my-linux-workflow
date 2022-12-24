4f9a48707acc051878f7df746be5ba7da7878113
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-12-23 22:21:41 -0300

    Download do REAPER para o arch

--- i-use-arch-btw.sh
+++ i-use-arch-btw.sh
@@ -2,13 +2,17 @@
 # ----------------------------- REQUISITOS ----------------------------- #
 
 LIST_OF_APPS=" git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git insomnia jre kazam kwin-bismuth latex-mk libvterm01 nvm rpi-imager spotify texlive-latexindent-meta tuxguitar tuxguitar-common upscayl-bin urw-arial visual-studio-code-bin yay yoshimi-git"
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+REAPER="https://www.reaper.fm/files/6.x/reaper672_linux_x86_64.tar.xz"
 
-
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+mkdir "$DIRETORIO_DOWNLOADS"
+wget -c "$REAPER" -P "$DIRETORIO_DOWNLOADS";
 
 ## Atualizando o repositório ##
 sudo pacman -Sy ;
 sudo pacman -S yay ;
- yay -S $LIST_OF_APPS --noconfirm
+yay -S $LIST_OF_APPS --noconfirm
 
 ## Instalando pacotes Flatpak ##
 flatpak install flathub com.bitwarden.desktop com.discordapp.Discord com.github.cfcurtis.pdfstitcher com.jetbrains.IntelliJ-IDEA-Community com.microsoft.Teams com.todoist.Todoist org.deluge_torrent.deluge org.flozz.yoga-image-optimizer org.gnome.EasyTAG org.telegram.desktop
