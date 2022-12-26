3676be3b61f96eaa2f7b166159983d06ffef53f3
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-12-26 16:58:58 +0000

    update para arch

--- i-use-arch-btw.sh
+++ i-use-arch-btw.sh
@@ -1,13 +1,18 @@
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
-LIST_OF_APPS=" git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git insomnia jre kazam kwin-bismuth latex-mk libvterm01 nvm rpi-imager spotify texlive-latexindent-meta tuxguitar tuxguitar-common upscayl-bin urw-arial visual-studio-code-bin yay yoshimi-git"
+LIST_OF_APPS=" git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git insomnia jre kazam kwin-bismuth latex-mk libvterm01 nvm rpi-imager spotify texlive-latexindent-meta tuxguitar upscayl-bin visual-studio-code-bin yay yoshimi-git "
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 REAPER="https://www.reaper.fm/files/6.x/reaper672_linux_x86_64.tar.xz"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$REAPER" -P "$DIRETORIO_DOWNLOADS";
+
+
+## Configuração do git 
+git config --global user.email "gabriel.victorc13@gmail.com";
+git config --global user.name "G4BR-13-L";
 
 ## Atualizando o repositório ##
 sudo pacman -Sy ;
@@ -23,8 +28,12 @@ sudo snap install authy;
 
 ## Desenvolvimento
 ### NODEJS
-yay -S nvm --noconfirm
-sudo nvm install --lts;
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+sudo nvm install v18;
 
 ## Rust
 yay -S cargo rust --noconfirm
