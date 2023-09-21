f8abf676bb3ad7d2190f1a52b743f59c8b3d655a
G4BR-13-L <gabriel.victorc13@gmail.com>
2023-09-21 17:22:44 -0300

    refatoração do script para archlinux

--- i-use-arch-btw.sh
+++ i-use-arch-btw.sh
@@ -1,47 +1,73 @@
+#!/bin/bash
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
-LIST_OF_APPS=" git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git kazam latex-mk libvterm01 nvmrpi-imager spotify texlive-latexindent-meta tuxguitar urw-arial visual-studio-code-bin yay yoshimi-git"
+# Lista de aplicativos a serem instalados
+LIST_OF_APPS="git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git kazam latex-mk libvterm01 nvmrpi-imager spotify texlive-latexindent-meta tuxguitar urw-arial visual-studio-code-bin yay yoshimi-git pipiwire-alsa pipewire-jack pipewire-pulse yabridge yabridgectl starship stremio"
 
-## Atualizando o repositório ##
-sudo pacman -Sy ;
-sudo pacman -S yay ;
+# ----------------------------- INSTALAÇÃO ----------------------------- #
+
+# Atualizar o repositório
+sudo pacman -Sy
+
+# Instalar o gerenciador de pacotes Yay
+if ! command -v yay &> /dev/null; then
+    echo "Instalando Yay..."
+    sudo pacman -S yay
+fi
+
+# Instalar os aplicativos da lista
+echo "Instalando aplicativos..."
 yay -S $LIST_OF_APPS --noconfirm
 
-## Instalando pacotes Flatpak ##
-flatpak install flathub com.bitwarden.desktop com.discordapp.Discord  com.microsoft.Teams org.deluge_torrent.deluge org.flozz.yoga-image-optimizer org.gnome.EasyTAG org.telegram.desktop
+# Instalar pacotes Flatpak
+flatpak_packages=("com.bitwarden.desktop" "com.discordapp.Discord" "com.microsoft.Teams" "org.deluge_torrent.deluge" "org.flozz.yoga-image-optimizer" "org.gnome.EasyTAG" "org.telegram.desktop")
 
-## Instalando snaps
-yay -S snapd --noconfirm
-sudo snap install authy;
+for package in "${flatpak_packages[@]}"; do
+    flatpak install flathub "$package"
+done
 
-## Desenvolvimento
-### NODEJS
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# Instalar Snapd e Authy
+if ! command -v snap &> /dev/null; then
+    echo "Instalando Snapd..."
+    yay -S snapd --noconfirm
+    sudo snap install authy
+fi
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# Instalar Node.js com NVM
+if ! command -v node &> /dev/null; then
+    echo "Instalando Node.js com NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    sudo nvm install v18
+fi
 
-sudo nvm install v18;
+# Instalar Rust e Cargo
+if ! command -v cargo &> /dev/null; then
+    echo "Instalando Rust e Cargo..."
+    yay -S cargo rust --noconfirm
+fi
 
-## Rust
-yay -S cargo rust --noconfirm
-
-# ### PYTHON
-yay -S python3
-pip install pip
+# Instalar Python3 e atualizar pip
+if ! command -v python3 &> /dev/null; then
+    echo "Instalando Python 3..."
+    yay -S python3
+fi
 pip install --upgrade pip
 
-
-# ###LATEX
-# sudo apt-get install texlive-full;
-
-### JAVA OPENJDK
-yay -S jdk --noconfirm
-
+# Instalar OpenJDK
+if ! command -v java &> /dev/null; then
+    echo "Instalando OpenJDK..."
+    yay -S jdk --noconfirm
+fi
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-## Finalização, atualização e limpeza##
+
+# Atualizar pacotes
+echo "Atualizando pacotes..."
 yay -Syu
-sudo reboot
-# ---------------------------------------------------------------------- #
+
+# Reiniciar o sistema
+echo "Reiniciando o sistema..."
+# sudo reboot
