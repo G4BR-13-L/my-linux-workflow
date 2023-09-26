d51ab0b81a15d1c7c4510a22bed45b011208cbea
Gabriel <gabriel.victorc13@gmail.com>
2023-09-26 19:13:12 -0300

    Melhorias no script e mais configurações

--- i-use-arch-btw.sh
+++ i-use-arch-btw.sh
@@ -2,29 +2,28 @@
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
-# Lista de aplicativos a serem instalados
-LIST_OF_APPS="git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git kazam latex-mk libvterm01 nvmrpi-imager spotify texlive-latexindent-meta tuxguitar urw-arial visual-studio-code-bin yay yoshimi-git pipiwire-alsa pipewire-jack pipewire-pulse yabridge yabridgectl starship stremio"
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 
 # ----------------------------- INSTALAÇÃO ----------------------------- #
 
 # Atualizar o repositório
 sudo pacman -Sy
 
-# Instalar o gerenciador de pacotes Yay
-if ! command -v yay &> /dev/null; then
-    echo "Instalando Yay..."
-    sudo pacman -S yay
-fi
+# Lista de aplicativos a serem instalados
+LIST_OF_APPS="git gimp vlc vim flatpak inkscape kdenlive curl libreoffice ranger steam abntex2 colorpicker google-chrome helm-git kazam latex-mk libvterm01 nvmrpi-imager spotify texlive-latexindent-meta tuxguitar urw-arial visual-studio-code-bin yoshimi-git pipiwire-alsa pipewire-jack pipewire-pulse yabridge yabridgectl starship stremio neofetch lapce spectacle filelight dbeaver steam noto-fonts-emoji obsidian reaper"
 
 # Instalar os aplicativos da lista
 echo "Instalando aplicativos..."
 yay -S $LIST_OF_APPS --noconfirm
 
 # Instalar pacotes Flatpak
-flatpak_packages=("com.bitwarden.desktop" "com.discordapp.Discord" "com.microsoft.Teams" "org.deluge_torrent.deluge" "org.flozz.yoga-image-optimizer" "org.gnome.EasyTAG" "org.telegram.desktop")
+flatpak_packages=("com.bitwarden.desktop" "com.discordapp.Discord" "com.microsoft.Teams" "org.deluge_torrent.deluge" "org.flozz.yoga-image-optimizer" "org.gnome.EasyTAG" "org.telegram.desktop" "com.jetbrains.IntelliJ-IDEA-Ultimate")
 
 for package in "${flatpak_packages[@]}"; do
-    flatpak install flathub "$package"
+    flatpak install flathub "$package" -y
 done
 
 # Instalar Snapd e Authy
@@ -60,6 +59,14 @@ pip install --upgrade pip
 if ! command -v java &> /dev/null; then
     echo "Instalando OpenJDK..."
     yay -S jdk --noconfirm
+fi
+
+# Instalar Docker
+if ! command -v java &> /dev/null; then
+    echo "Instalando Docker..."
+    yay -S docker --noconfirm
+    sudo systemctl enable docker
+    sudo systemctl start docker
 fi
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
